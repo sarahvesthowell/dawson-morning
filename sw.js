a66a359b989fc61393545bca2dcb353c562b0f3d
Sarah Howell <sarah@costallocationpro.com>
2026-09-09 11:17:06 -0400

    Link Calm Corner from main screen with easy two-way navigation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'dawson-morning-v6';
+const CACHE = 'dawson-morning-v7';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
