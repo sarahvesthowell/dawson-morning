3ecbdb78f44bb8788807bf92093431c9cedd6074
Sarah Howell <sarah@costallocationpro.com>
2026-09-09 16:06:06 -0400

    Add chore list page with persistent daily progress and cross-nav

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'dawson-morning-v7';
-const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+const CACHE = 'dawson-morning-v8';
+const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png', './calm.html', './chores.html'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
