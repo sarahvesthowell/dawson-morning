2970732ff0feb1dc4cd6e35c3f39de255751c205
Sarah Howell <sarah@costallocationpro.com>
2026-09-09 10:48:37 -0400

    Move get dressed deadline to 6:35

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'dawson-morning-v5';
+const CACHE = 'dawson-morning-v6';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
